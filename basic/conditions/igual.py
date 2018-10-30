6a0ba24140d87fae06c858934a6bf74e0171cdb6
Roberto Bautista <robetobau45@gmail.com>
2018-10-30 16:31:53 -0600

    Agregar ejemplos de condiciones

---
+++ basic/conditions/igual.py
@@ -0,0 +1,8 @@
+input_1 = input('Ingrese numero 1 = ')
+num_1 = int(input_1)
+input_2 = input('Ingrese numero 2 = ')
+num_2 = int(input_2)
+if num_1 == num_2:
+    print('Estos numeros son iguales')
+else:
+    print('Estos numeros no son iguales')
---
+++ basic/conditions/mayor.py
@@ -0,0 +1,8 @@
+input_1 = input('Ingrese numero 1 = ')
+num_1 = int(input_1)
+input_2 = input('Ingrese numero 2 = ')
+num_2 = int(input_2)
+if num_1 > num_2:
+    print(num_1,'es el numero mas grande.')
+else:
+    print(num_2,'es el numero mas grande.')
