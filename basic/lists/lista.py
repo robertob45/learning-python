b410ad05eba24dc524467a890e107a3c0e454cfa
Roberto Bautista <robetobau45@gmail.com>
2018-10-30 16:33:39 -0600

    Agregar ejemplos de lista

---
+++ basic/lists/lista.py
@@ -0,0 +1,8 @@
+lista = [1,2,3,4,5]
+print(lista[0])
+print(lista[-2])
+print(lista.pop()) # Elimina el ultimo valor y lo devuelve
+print(lista.append(3))# Añade al final de la lista este valor
+print(lista.insert(3,10))# Añade el segundo valor en la posicion indicada en al primer valor
+print(lista)
+print(lista[1:4])
