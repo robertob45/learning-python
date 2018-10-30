b108b517a31faa746df715c9b9d5d87204415d27
Roberto Bautista <robetobau45@gmail.com>
2018-10-30 16:34:06 -0600

    Agregar ejemplos de operaciones

---
+++ basic/operations/multiplicacion.py
@@ -0,0 +1,6 @@
+input_1 = input('Ingrese numero 1 = ')
+num_1 = int(input_1)
+input_2 = input('Ingrese numero 2 = ')
+num_2 = int(input_2)
+multip_total = num_1 * num_2
+print(multip_total)
---
+++ basic/operations/suma.py
@@ -0,0 +1,7 @@
+# Convertimos los valores en numeros
+input_1 = input('Ingrese numero 1 = ')
+num_1 = int(input_1)
+input_2 = input('Ingrese numero 2 = ')
+num_2 = int(input_2)
+suma_total = num_1 + num_2
+print(suma_total)
