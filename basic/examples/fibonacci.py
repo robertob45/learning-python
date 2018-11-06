ef0ab05a52958e95d17c612ccb947d5d928fa9d5
Roberto Bautista <robetobau45@gmail.com>
2018-11-06 09:43:22 -0600

    Modificado ejemplo de while

--- basic/examples/fibonacci.py
+++ basic/examples/fibonacci.py
@@ -1,7 +1,11 @@
-fin = int(input('Fin de la sucesión: '))
+num = int(input('Ingrese número: '))
 
 fib = 0
 fib_2 = 1
-while fib_2 < fin:
+
+while fib_2 < num:
+    #fib = fib_2
+    #fib_2 = fib + fib_2
     fib, fib_2 = fib_2, fib + fib_2
-    print(fib)
+
+print('El numero de fibonacci de', num, 'es:', fib)
