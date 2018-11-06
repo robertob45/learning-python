16f06b984812d8c996740981bb2fd5d8cddb5fcc
Roberto Bautista <robetobau45@gmail.com>
2018-11-06 09:47:19 -0600

    Añadido ejemplo de strings

---
+++ basic/strings/methods.py
@@ -0,0 +1,10 @@
+var = 'Hola'
+print(var[3])
+print(len(var))
+var = var + ' mundo'
+print(var)
+print(var[1:5])
+print(var.lower())
+print(var.upper())
+print(var.strip())
+print(var.replace(' ', '+'))
