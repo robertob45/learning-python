7407f7d9e513792150eb2b65ebc644b5f8632c56
Roberto Bautista <robertobau45@gmail.com>
2018-12-10 12:52:53 -0600

    Modificado proyecto 3

--- miniProjects/Project3.py
+++ miniProjects/Project3.py
@@ -1,33 +1,39 @@
 import time
+import random
+import sys
 
 def start_game():
     letters = 'abcdefghijklmnopqrstuvwxyz'
-    word = 'hello'
-    word_len = len(word)
+    wordList = ["lion", "umbrella", "window", "computer", "glass", "juice", "chair", "desktop",
+ "laptop", "dog", "cat", "lemon", "cabel", "mirror", "hat"]
+    secretWord = random.choice(wordList)
+    word_len = len(secretWord)
     used_letters = []
     print('Hello! :)\n')
-    time.sleep(2)
+    # time.sleep(2)
     print('The word you have to guess has', word_len, 'characters\n')
-    time.sleep(2)
+    # time.sleep(2)
     print('You can only enter 1 letter from a - z\n')
-    time.sleep(2)
+    # time.sleep(2)
     print('Lets start! :)\n')
-    time.sleep(2)
-    guessing(letters, used_letters, word, word_len)
+    # time.sleep(2)
+    guessing(letters, used_letters, secretWord, word_len)
 
 
-def guessing(letters, used_letters, word, word_len):
+def guessing(letters, used_letters, secretWord, word_len):
     correct_lett = []
     missed_lett = []
     blank_spaces = '_' * word_len
     print(blank_spaces, '\n')
-    attempts = 3
+    attempts = 10
     print('You have:', attempts, 'attempts')
     print('')
     while attempts > 0:
-        guess = input('Enter a letter')
+        guess = input('Enter a letter (Write "exit", to stop playing):\n')
         print('')
-        if not guess in letters:
+        if guess == 'exit':
+            sys.exit('Ok, goodbye! :)')
+        elif not guess in letters:
             print('Thats not a valid letter!')
         elif guess == '':
             print('No blank spaces!')
@@ -36,33 +42,34 @@ def guessing(letters, used_letters, word, word_len):
         else:
             used_letters.append(guess)
             correct_lett.append(guess)
-            if guess in word:
+            if guess in secretWord:
                 print('You got one!\n')
                 for x in range(word_len):
-                    if word[x] in correct_lett:
-                        blank_spaces = blank_spaces[:x] + word[x] + blank_spaces[x+1:]
+                    if secretWord[x] in correct_lett:
+                        blank_spaces = blank_spaces[:x] + secretWord[x] + blank_spaces[x+1:]
                 for letter in blank_spaces:
                     print(letter, end=' ')
                 print('')
                 print('')
                 foundAllLetters = True
                 for y in range(word_len):
-                    if not word[y] in correct_lett:
+                    if not secretWord[y] in correct_lett:
                         foundAllLetters = False
                 if foundAllLetters:
-                    print('You won! :D')
-                    playAgain = input('Want to play again? :) y/n')
+                    print('You won! :D\n')
+                    playAgain = input('Want to play again? :) y/n: ')
+                    print('')
                     if playAgain == 'y':
                         start_game()
                     else:
-                        print('Goodbye! ;)')
-                        break
+                        sys.exit('Goodbye! ;)')
             else:
                 print('That letter is not in the word!, -1 try')
+                print('You have', attempts, 'attempts left')
                 missed_lett.append(guess)
                 attempts -= 1
                 if attempts == 0:
-                    print('Sorry you lost :(, the word was:', word)
+                    print('Sorry you lost :(, the word was:', secretWord)
 
 # def won(correct_lett):
 #     foundAllLetters = True
